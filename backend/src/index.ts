819b8c10d2e7ac5f113577c663d6c5ce44bd5236
MohitGupta <krishgupta1630@gmail.com>
2024-02-07 21:48:16 +0530

    adding add hotel api and hotel model

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,20 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose"
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 import "dotenv/config";
 
 // IMPORTING ROUTES
 import userRoutes from "./routes/user-route";
 import authRoutes from "./routes/auth-route";
+import myHotelsRoutes from "./routes/myHotels-route";
+
+// CLOUDINARY CONFIG
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // DATABASE CONNECTION
 mongoose.connect(process.env.MONGODB_CONNECTION_URL as string);
@@ -26,6 +35,7 @@ app.use(express.urlencoded({extended: true}));
 // ROUTES
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/my-hotels", myHotelsRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
---
+++ backend/src/routes/myHotels-route.ts
@@ -0,0 +1,79 @@
+import express, { Request, Response } from "express";
+import multer from "multer";
+import cloudinary from "cloudinary";
+import Hotel from "../models/hotel-models";
+import { verifyToken } from "../middlewares/verifyToken";
+import { body } from "express-validator";
+import { HotelType } from "../shared/types";
+
+const router = express.Router();
+
+// MULTER CONFIGURATION
+const storage = multer.memoryStorage();
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5MB
+    },
+});
+
+// ADD HOTEL ROUTE
+// path     /api/my-hotels
+
+router.post("/", 
+    verifyToken,
+    [
+        // Same as check(), but only validates req.body.
+        body("name").notEmpty().withMessage("Name is required"),
+        body("city").notEmpty().withMessage("City is required"),
+        body("country").notEmpty().withMessage("Country is required"),
+        body("description").notEmpty().withMessage("Description is required"),
+        body("type").notEmpty().withMessage("Hotel type is required"),
+        body("pricePerNight").notEmpty().isNumeric().withMessage("Price is required and must be a number"),
+        body("facilities").notEmpty().isArray().withMessage("Facilities are required"),
+    ], 
+    upload.array("imageFiles", 6), 
+    async (req:Request, res:Response) => {
+    // Main Login Here...
+    try {
+        const imageFiles = req.files as Express.Multer.File[];
+        const newHotelData:HotelType = req.body;
+        
+        // 1. Upload the image files to Cloudinary
+        const imageUrls = await uploadImages(imageFiles);
+
+        // 2. If upload was successful, add the URLs to the new hotel
+        newHotelData.imageUrls = imageUrls;
+        newHotelData.lastUpdated = new Date();
+        newHotelData.userId = req.userId;
+
+        // 3. Save the new hotel in database
+        const hotel = new Hotel(newHotelData);
+        await hotel.save();
+
+        // 4. Return a success response
+        res.status(201).json(hotel);
+    } catch (error) {
+        res.status(500).json({message: "Something went wrong"});
+    }
+});
+
+// FUNCTION TO UPLOAD IMAGES TO CLOUDINARY AND GET BACK THE URL
+async function uploadImages (imageFiles: Express.Multer.File[]) {
+    const uploadPromises = imageFiles.map(async (image) => {
+        // Converts per images into buffer data to a Base64-encoded string
+        const b64 = Buffer.from(image.buffer).toString("base64");
+        // create a Data URI 
+        let dataURI = "data:" + image.mimetype + ";base64," + b64;
+        // res has url which comes from cloudinary
+        const res = await cloudinary.v2.uploader.upload(dataURI);
+        return res.url;
+    });
+
+    const imageUrls = await Promise.all(uploadPromises);
+    return imageUrls;
+};
+
+
+export default router;
